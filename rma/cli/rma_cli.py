cd5211e4495272b5cff6a7793c0a7a74551d6375
javalover123 <javalover123@foxmail.com>
2022-04-15 14:43:36 +0800

    Add default example type

--- rma/cli/rma_cli.py
+++ rma/cli/rma_cli.py
@@ -70,6 +70,7 @@ def main():
                         help="Specify application working mode. Allowed values are " + ', '.join(VALID_MODES))
     parser.add_argument("-t", "--type",
                         dest="types",
+                        # default=["string"],
                         action="append",
                         help="""Data types to include. Possible values are string, hash, list, set.
                               Multiple types can be provided. If not specified, all data types will be returned.
